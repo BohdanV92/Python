ad1fd538334d9491ba7bc0f355a652822053f25d
Bohdan Vorobets <81592637+BohdanV92@users.noreply.github.com>
2021-05-14 17:34:59 +0300

    Update Hw6.5.py

--- HW6/Hw6.5.py
+++ HW6/Hw6.5.py
@@ -28,18 +28,3 @@ for item in list1:
             list1.remove(item)
 
 print ("List_changed: ", list1)
-
-
-"""
-# Як в for пройтись ззаду наперед по list1 ? ...тоді елементи не зсувались б на інші індекси...
-
-list1 = []
-
-for item in range (20):
-    list1.append(random.randint(-100, 100))
-print ("List1: ", list1)
-for item in list1 (??? -1 ???):
-    if item < 0:
-        list1.remove(item)
-print ("List_changed: ", list1)
-"""
